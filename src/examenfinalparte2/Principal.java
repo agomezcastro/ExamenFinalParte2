7b9ad0d8d09a40157dc500ce66a4e52871d9425a
agomezcastro <agomezcastro@atanasoff04.danielcastelao.org>
2016-06-06 09:23:17 +0200

    Nuevo metodo variables cambiadas

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -3,57 +3,61 @@ package examenfinalparte2;
 public class Principal {
 
     public static boolean p = false;
-
+ 
     public static void main(String arg[]) {
-        int dig = 2;
-        int ndig = 0;
-        if (dig <= 0) {
+        int digitos = 2;
+        int numeroDigitos = 0;
+        if (digitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
-            int aux = i;
+            int auxiliar = i;
 
             int contador = 0;
 
-            while (aux != 0) {
-                aux = aux / 10;
+            while (auxiliar != 0) {
+                auxiliar = auxiliar / 10;
                 contador++;
             }
-            ndig = contador;
+            numeroDigitos = contador;
 
-            if (ndig == dig) {
-                if (i < 4) {
-                    p = true;
+            Comprobar(numeroDigitos, digitos, i);
+        }
+    }
+
+    public static void Comprobar(int numeroDigitos, int digitos, int i) {
+        if (numeroDigitos == digitos) {
+            if (i < 4) {
+                p = true;
+            } else {
+                if (i % 2 == 0) {
+                    p = false;
                 } else {
-                    if (i % 2 == 0) {
-                        p = false;
-                    } else {
-                        int contador1 = 0;
-                        int i1 = 1;
-                        int k = (i - 1) / 2;
-                        if (k % 2 == 0) {
-                            k--;
+                    int contador1 = 0;
+                    int i1 = 1;
+                    int k = (i - 1) / 2;
+                    if (k % 2 == 0) {
+                        k--;
+                    }
+                    
+                    while (i1 <= k) {
+                        if (i % i1 == 0) {
+                            contador1++;
                         }
-
-                        while (i1 <= k) {
-                            if (i % i1 == 0) {
-                                contador1++;
-                            }
-                            i1 += 2;
-                            if (contador1 == 2) {
-                                i1 = k + 1;
-                            }
-                        }
-
-                        if (contador1 == 1) {
-                            p = true;
+                        i1 += 2;
+                        if (contador1 == 2) {
+                            i1 = k + 1;
                         }
                     }
+                    
+                    if (contador1 == 1) {
+                        p = true;
+                    }
                 }
-
-                if (p == true) {
-                    System.out.println(i);
-                }
+            }
+            
+            if (p == true) {
+                System.out.println(i);
             }
         }
     }
